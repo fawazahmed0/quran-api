f093d521fad29eb6cbe4f6eb8299a5f5f6667a98
Fawaz Ahmed <fawazahmed0@hotmail.com>
2020-10-15 04:30:20 +0530

    change latingen back to true

--- apiscript.js
+++ apiscript.js
@@ -4,7 +4,7 @@ var checkduplicate = true;
 // whether json is required in translation or not
 var jsonrequired = true
 // whether to generate latin or not using translate.py script
-var generateLatin = false
+var generateLatin = true
 
 const fs = require('fs');
 const path = require('path');
