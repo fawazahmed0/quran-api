80f91c011497badd0c6fb496600d589f2bcf54df
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-11-22 11:26:09 +0530

    Update arabicdiacriticsremover.js

--- scripts/files/datacleaning/arabicdiacriticsremover.js
+++ scripts/files/datacleaning/arabicdiacriticsremover.js
@@ -17,6 +17,7 @@ async function begin(){
 
   for(let file of fs.readdirSync(pathToDiacritics)){
     let str =  fs.readFileSync(path.join(pathToDiacritics, file)).toString()
+                                                                                                    // replacing hamza wasl with alif, it may seem opposite on looking
     str = str.normalize("NFD").replace(/\p{Diacritic}|\p{Mark}|\p{Extender}|\p{Bidi_Control}/gu, "").replaceAll('ٱ','ا')
     fs.writeFileSync(path.join(pathToSave, file),str)
   }
