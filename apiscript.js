c4896e3b9338f6ef989dd7838075f0196c7a9a35
Fawaz Ahmed <fawazahmed0@hotmail.com>
2023-03-23 04:25:33 +0530

    unescape >

--- apiscript.js
+++ apiscript.js
@@ -492,7 +492,7 @@ function cleanTrans(arr) {
   for (i = 0; i < arr.length; i++) {
     // https://en.wikipedia.org/wiki/List_of_Unicode_characters#Basic_Latin
     // This will remove all special symbols and numbers from starting and ending of verse
-    arr[i] = arr[i].replace(/^[\u0020-\u0040|\u005b-\u0060|\u007b-\u007e|\s|\n|\r|\p{N}]{1,20}/u, " ").replace(/^\s*\w{1}\s*(\.|\)|\}|\>|\])+[\u0020-\u0040|\u005b-\u0060|\u007b-\u007e|\s|\n|\r|\p{N}]{0,7}/ui, " ").replace(/[\u0020-\u0040|\u005b-\u0060|\u007b-\u007e|\s|\n|\r|\p{N}]{1,15}$/u, " ").replace(/[\r\n]/g, " ").replace(/\s\s+/g, " ").trim()
+    arr[i] = arr[i].replace(/^[\u0020-\u0040|\u005b-\u0060|\u007b-\u007e|\s|\n|\r|\p{N}]{1,20}/u, " ").replace(/^\s*\w{1}\s*(\.|\)|\}|>|\])+[\u0020-\u0040|\u005b-\u0060|\u007b-\u007e|\s|\n|\r|\p{N}]{0,7}/ui, " ").replace(/[\u0020-\u0040|\u005b-\u0060|\u007b-\u007e|\s|\n|\r|\p{N}]{1,15}$/u, " ").replace(/[\r\n]/g, " ").replace(/\s\s+/g, " ").trim()
     // Checking partially open/close bracket exists or not at begninning of verse
     var bracket1 = arr[i].match(/^[^\[|\(|\<|\{]+(\]|\)|\>|\})/)
     // Checking partially open/close bracket exists or not at end of verse
